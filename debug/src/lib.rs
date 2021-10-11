81145201ea9742313c55da839106b6a6e255b87d
hacpy <hacpy.lbx@gmail.com>
2021-10-11 20:38:59 +0800

    Pass debug test 2

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,35 @@
-use proc_macro2::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    eprintln!("input:{:#?}", input);
-    TokenStream::new().into()
+    let name = input.ident;
+    let data = input.data;
+    let fields = match &data {
+        Data::Struct(data) => data.fields.to_owned(),
+        Data::Enum(_) => todo!(),
+        Data::Union(_) => todo!(),
+    };
+
+    let new_fields = fields.iter().map(|field| {
+        let name = &field.ident.to_owned().unwrap();
+        let name_str = name.to_string();
+        quote! {
+            .field(#name_str, &self.#name)
+        }
+    });
+
+    let name_str = name.to_string();
+    let expand = quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+                fmt.debug_struct(#name_str)
+                    #( #new_fields )*
+                    .finish()
+            }
+        }
+    };
+
+    expand.into()
 }
--- main.rs
+++ main.rs
@@ -5,5 +5,12 @@
 //
 // To run the code:
 //     $ cargo run
+use derive_debug::CustomDebug;
+
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u16,
+}
 
 fn main() {}

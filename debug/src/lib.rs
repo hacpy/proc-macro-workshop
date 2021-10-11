649e4af5687d5e40ef0fddcbd3ac4ea6f102d1df
hacpy <hacpy.lbx@gmail.com>
2021-10-11 18:06:05 +0800

    Init debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,9 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    eprintln!("input:{:#?}", input);
+    TokenStream::new().into()
 }
